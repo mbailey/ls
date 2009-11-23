5c254f8dd825f650580306ed76302417f451787e
Mike Bailey <mike@bailey.net.au>
2009-11-23 20:49:59 +1100

    Added placement controller methods

--- app/controllers/admin/placements_controller.rb
+++ app/controllers/admin/placements_controller.rb
@@ -1,8 +1,55 @@
 class Admin::PlacementsController < Admin::BaseController
 
-  def index
+  def search
     @search = PlacementSearch.new(params[:search])
     @volunteers = @search.all#.paginate(:page => params[:page])
+  end
+  
+  def index
+    @placements = Placement.all
+  end
+  
+  def show
+    @placement = Placement.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @home_check }
+    end
+  end
+  
+  def new
+    @placement = Placement.new
+  end
+  
+  def create
+    @placement = Placement.new(params[:placement])
+
+    respond_to do |format|
+      if @placement.save
+        flash[:notice] = 'Placement was successfully created.'
+        format.html { redirect_to([:admin, @placement]) }
+        format.xml  { render :xml => @placement, :status => :created, :location => @placement }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @placement.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @placement = Placement.find(params[:id])
+
+    respond_to do |format|
+      if @placement.update_attributes(params[:placement])
+        flash[:notice] = 'Placement was successfully updated.'
+        format.html { redirect_to([:admin, @placement]) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @placement.errors, :status => :unprocessable_entity }
+      end
+    end
   end
 
 end
