b72e8a6eb0a3774b281c5b191be0be9139fc1a52
Mike Bailey <mike@bailey.net.au>
2009-12-14 23:51:45 +1100

    Don't allow two capabilities with same name

--- app/models/capability.rb
+++ app/models/capability.rb
@@ -2,4 +2,6 @@ class Capability < ActiveRecord::Base
   has_and_belongs_to_many :carers, :uniq => true
   has_and_belongs_to_many :animals, :uniq => true
   has_and_belongs_to_many :placements, :uniq => true
+  
+  validates_uniqueness_of :name
 end
